2b9c4b968bad7c0605cb0159d92cf646b4fd140e
sunandan <sunandanlalka@gmail.com>
2022-11-27 19:47:24 +0530

    sections reduced

--- src/screens/results/Results.jsx
+++ src/screens/results/Results.jsx
@@ -4,8 +4,6 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 import CircleProgress from '../../components/circleprogress/CircleProgress'
 import { HiQuestionMarkCircle } from 'react-icons/hi';
 import { FcCheckmark } from 'react-icons/fc';
-import { FaTimes } from 'react-icons/fa';
-import { BsExclamationTriangle } from 'react-icons/bs';
 import {useLocation} from 'react-router-dom';
 
 var root = document.querySelector(':root');
@@ -74,7 +72,7 @@ const Results = () => {
     <div style={{ height: '100vh', width: '100vw' }} className='container-fluid p-0 m-0 bg-details'>
 
       <div className='row m-0 p-0 bg-details'>
-        <div style={{ backgroundColor: 'white', height: '100vh'}} className="col-2 px-1 d-flex mx-auto flex-column justify-content-center">
+        <div style={{ backgroundColor: 'white', height: '100vh'}} className="col-2 px-1 d-flex mx-auto size flex-column justify-content-center">
           <h2 className='mx-auto mb-5 text-dark'>Match Rate</h2>
           <div style={{position:'relative'}} className="circle-wrap d-flex align-items-center justify-content-center">
             <CircleProgress  progress={p6} strokeColor={variant6} />
@@ -121,50 +119,6 @@ const Results = () => {
 
         </div>
         <div className="col-10 px-5">
-          <div className="row py-3 mt-4">
-            <div className="col">
-              <h2>Searchability</h2>
-              <p className='text-secondary'>Applicant Tracking Systems (ATS) are computers that process your resume to understand your work history and relevance to the job description. These findings typically include your work history, job titles, relevant skills and education, as well as contact information like your name, phone number, and email address.
-                Tip: Complete all checks below to ensure your resume is easily searchable by recruiters and ATS.</p>
-              <div className="table-wrap">
-                <table className="table">
-                  <tbody >
-                    <tr>
-                      <td className='py-3 px-4'>ATS Tip</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-success'><FcCheckmark /></td>
-                      <td className='text-secondary'></td>
-
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Job title match</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-success'><FcCheckmark /></td>
-                      <td className='text-secondary'></td>
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Education match</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-success'><FcCheckmark /></td>
-                      <td className='text-secondary'>This job requires or prefers a Bachelor's degree. A Bachelor's degree is found in your resume.</td>
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Section headings</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='py-2 text-secondary'>Name your experience section “Work History" or “Professional Experience” for ATS to recognize work sections.</td>
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Date formatting</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-success'><FcCheckmark /></td>
-                      <td className='text-secondary'>The dates in your work experience section are properly formatted.</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
           <div className="row py-3">
             <div className="col">
               <h3>Hard skills</h3>
@@ -237,106 +191,8 @@ const Results = () => {
               </div>
             </div>
           </div>
-          <div className="row py-3">
-            <div className="col">
-              <h3>Other keywords</h3>
-              <p className='text-secondary'>Other keywords are words included in the job description more than 3 times and not hard skills or soft skills. These words are typically buzzwords, industry lingo, or company specific jargon that may be unique to the specific company and help your resume get noticed. Other keywords have a low impact on your match score.
-
-                Tip: Spend less time including these in your resume on a case-by-case basis.</p>
-              <div className="table-wrap">
-                <table class="table">
-                  <thead>
-                    <tr>
-                      <td className='py-2 px-4 text-secondary'>Skill</td>
-                      <td className='text-secondary'>Resume</td>
-                      <td className='text-secondary'>Job description</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td className='py-2 px-4'>Interface</td>
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>1</td>
-
-                    </tr>
-                    <tr>
-                      <td className='py-2 px-4'>User</td>
-
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>1</td>
-                    </tr>
-                    <tr>
-                      <td className='py-2 px-4'>Developer</td>
-
-                      <td className='text-secondary'>2</td>
-                      <td className='text-secondary'>6</td>
-                    </tr>
-                    <tr>
-                      <td className='py-2 px-4'>Concepts</td>
-
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>8</td>
-                    </tr>
-                    <tr>
-                      <td className='py-2 px-4'>React</td>
-
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>7</td>
-                    </tr>
-                    <tr>
-                      <td className='py-2 px-4'>Components</td>
-
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>5</td>
-                    </tr>
-                    <tr>
-                      <td className='py-2 px-4'>Implementing</td>
-
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>3</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          <div className="row py-3">
-            <div className="col">
-              <h3 className='py-2'>Recruiter tips</h3>
-              <div className="table-wrap">
-                <table class="table">
-                  <tbody>
-                    <tr>
-                      <td className='py-3 px-4'>Word count</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-secondary'><FcCheckmark /></td>
-                      <td className='text-secondary'>There are 156 words in your resume, which is under the suggested 1000 word count for relevance and ease of reading reasons.
-                      </td>
-
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Measurable reults</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-warning'><BsExclamationTriangle /></td>
-                      <td className='text-secondary'></td>
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Job level match</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-secondary'><FcCheckmark /></td>
-                      <td className='text-secondary'>You are applying to a(n) junior level role. Given your 2 years of experience, this role is a great fit for your experience.</td>
-                    </tr>
-                    <tr>
-                      <td className='py-3 px-4'>Words to avoid</td>
-                      <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-secondary'><FcCheckmark /></td>
-                      <td className='text-secondary'></td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
+          
+  
           <div className="row py-3">
             <div className="col">
               <h3 className='py-2'>Formatting</h3>
@@ -346,15 +202,15 @@ const Results = () => {
                     <tr>
                       <td className='py-3 px-4'>Page size</td>
                       <td className='text-secondary'><HiQuestionMarkCircle /></td>
-                      <td className='text-danger'><FaTimes /></td>
-                      <td className='text-secondary'>the page size should be standard sized for example A4</td>
+                      <td className='text-danger'><FcCheckmark /></td>
+                      <td className='text-secondary'>the page size is standard sized i.e. A4</td>
 
                     </tr>
                     <tr>
                       <td className='py-3 px-4'>Bold font</td>
                       <td className='text-secondary'><HiQuestionMarkCircle /></td>
                       <td className='text-secondary'><FcCheckmark /></td>
-                      <td className='text-secondary'></td>
+                      <td className='text-secondary'>Bold font is added in between</td>
                     </tr>
                   </tbody>
                 </table>
